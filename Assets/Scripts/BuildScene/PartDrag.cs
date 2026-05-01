4875b03fa9e3c3f0c87b2da0e94be08ed176e57e
bananacat50 <elllliotttt@gmail.com>
2026-05-01 03:58:17 -0700

    Fixed issues with dragging stackable parts

--- Assets/Scripts/BuildScene/PartDrag.cs
+++ Assets/Scripts/BuildScene/PartDrag.cs
@@ -201,6 +201,7 @@ public class PartDrag : MonoBehaviour {
         
         if(partDB.PartIsStackable(part)) {
             SetSortingLayer(stackablePartLayer, part);
+            part.transform.position += new Vector3(0,0,-1);
             shipGrid.partStackedOn[part] = shipGrid.GetPlacedPartByWorldPosition(worldPosition);
         }
         else SetSortingLayer(defaultLayer, part);
