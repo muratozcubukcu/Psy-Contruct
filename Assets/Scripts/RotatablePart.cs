aa008866cfa9e68ab476a29ef26970b6532c7cbb
ChocolateMilk71 <olliepurdy@comcast.net>
2026-05-19 23:56:55 -0700

    stupid fucking code

--- Assets/Scripts/RotatablePart.cs
+++ Assets/Scripts/RotatablePart.cs
@@ -17,25 +17,25 @@ public class RotatablePart : MonoBehaviour {
     
     public void SetRotation(ShipBuildingGrid.direction newConnectingDirection) {
         connectingDirection = newConnectingDirection;
-        TextMeshProUGUI tmp = GetComponentInChildren<TextMeshProUGUI>();
-        Quaternion rotation = new Quaternion();
         
+        TextMeshProUGUI tmp = GetComponentInChildren<TextMeshProUGUI>();;
         switch (newConnectingDirection) {
             case ShipBuildingGrid.direction.above:
-                rotation = Quaternion.Euler(0, 0, 180);
+                transform.localRotation = Quaternion.Euler(0, 0, 180);
+                if(tmp != null) tmp.gameObject.GetComponent<RectTransform>().localRotation = Quaternion.Euler(0, 0, 180);
                 break;
             case ShipBuildingGrid.direction.below:
-                rotation = Quaternion.Euler(0, 0, 0);
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+                if(tmp != null) tmp.gameObject.GetComponent<RectTransform>().localRotation = Quaternion.Euler(0, 0, 0);
                 break;
             case ShipBuildingGrid.direction.left:
-                rotation = Quaternion.Euler(0, 0, -90);
+                transform.localRotation = Quaternion.Euler(0, 0, -90);
+                if(tmp != null) tmp.gameObject.GetComponent<RectTransform>().localRotation = Quaternion.Euler(0, 0, 90);
                 break;
             case ShipBuildingGrid.direction.right:
-                rotation = Quaternion.Euler(0, 0, 90);
+                transform.localRotation = Quaternion.Euler(0, 0, 90);
+                if(tmp != null) tmp.gameObject.GetComponent<RectTransform>().localRotation = Quaternion.Euler(0, 0, -90);
                 break;
         }
-        
-        transform.localRotation = rotation;
-        if(tmp != null) tmp.gameObject.GetComponent<RectTransform>().localRotation = rotation;
     }
 }
