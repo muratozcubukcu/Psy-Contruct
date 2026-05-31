a976dbc1c78493c1fc06cbf066f5d29a49a263d7
bananacat50 <elllliotttt@gmail.com>
2026-05-30 19:27:17 -0700

    made sensors glow when they are active (when having won)

--- Assets/Scripts/SpacecraftParts/MultispectralImager.cs
+++ Assets/Scripts/SpacecraftParts/MultispectralImager.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
-
-//Class defines the behavior of the multispectral imager part. 
+using UnityEngine.SceneManagement;
+//Class defines the behavior of the gamma ray part. 
 
 public class MultispectralImager : MonoBehaviour
 {
+
     [SerializeField] private Spacecraft spacecraft;
+    [SerializeField] private Sprite defaultSprite;
+    [SerializeField] private Sprite glowSprite;
 
-    public void Awake() => enabled = false;
+    // Start with the component turned off
+    public void Awake()
+    {
+        enabled = false;
+        OrbitAssist.OnEnteredOrbit += activateGlow;
+        SceneManager.sceneLoaded += deactivateGlow;
 
-    // No behavior yet � just a marker component.
+    } 
+    // Update is called once per frame
     void Update()
     {
+        
+    }
 
+    void activateGlow(object sender, System.EventArgs e)
+    {
+        GetComponentInChildren<SpriteRenderer>().sprite = glowSprite;
+    }
+
+    void deactivateGlow(Scene scene, LoadSceneMode mode)
+    {
+        GetComponentInChildren<SpriteRenderer>().sprite = defaultSprite;
+    }
+
+    void OnDestroy()
+    {
+        OrbitAssist.OnEnteredOrbit -= activateGlow;
+        SceneManager.sceneLoaded -= deactivateGlow;
     }
 }
